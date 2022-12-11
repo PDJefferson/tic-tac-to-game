52a39e89b19c9b6ecc0d56209e53e2ac3d1a3489
PDJefferson <djefferson545@gmail.com>
2022-12-11 15:49:19 -0800

    refactored randomize algo selection logic.

--- my-app/utils/randomizeAlgoSelection.js
+++ my-app/utils/randomizeAlgoSelection.js
@@ -1,10 +1,7 @@
-import minimaxAlgo from "./minimaxAlgo"
-import randomPositionNotTaken from "./randomStep"
+import minimaxAlgo from './minimaxAlgo'
+import randomPositionNotTaken from './randomStep'
 
 export default function randomizeALgoSelection(board) {
-    let randomChoice = Math.floor(Math.random() * (1 - 0 + 1)) + 0
-    if(randomChoice === 1) {
-        return minimaxAlgo(board)
-    }
-    return randomPositionNotTaken(board)
+  let randomChoice = Math.floor(Math.random() * (1 - 0 + 1)) + 0
+  return randomChoice === 1 ? minimaxAlgo(board) : randomPositionNotTaken(board)
 }
